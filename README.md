315217fdd13079ab5ce289beed8a179e9c845fd3
André Bauer <abauer@magix.net>
2016-12-17 12:10:51 +0100

    addind docker-compose

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# zammad-docker-compose
+Welcome to Zammad
+=================
+
+Zammad is a web based open source helpdesk/ticket system with many features
+to manage customer communication via several channels like telephone, facebook,
+twitter, chat and e-mails. It is distributed under the GNU AFFERO General Public
+ License (AGPL) and tested on Linux, Solaris, AIX, FreeBSD, OpenBSD and Mac OS
+10.x. Do you receive many e-mails and want to answer them with a team of agents?
+You're going to love Zammad!
+
+What is zammad-docker-compose repo for?
+----------------------------------------
+
+This repo is meant to be the starting point for somebody who likes to test zammad.
+If you like to run zammad in production you should use one of the DEB or RPM packages.
+
+Getting started with zammad-docker-compose
+------------------------------------------
+
+* sysctl -w vm.max_map_count=262144
+* docker-compose up --build
+
+Docs
+----
+
+https://docs.zammad.org/en/latest/contributing-install-docker.html
---
+++ docker-entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ZAMMAD_DIR="/home/zammad"
+DEBUG="no"
+
+export RAILS_ENV="production"
+
+if [ "$1" = 'zammad' ]; then
+
+    # get zammad
+    if [ -f ${ZAMMAD_DIR}/config/database.yml ]; then
+	echo "updating zammad..."
+	cd ${ZAMMAD_DIR}
+	git pull
+	bundle update
+	rake db:migrate
+    else
+	echo "installing zammad..."
+	cd /tmp
+	git clone https://github.com/zammad/zammad.git
+	shopt -s dotglob
+	mv -f /tmp/zammad/* ${ZAMMAD_DIR}/
+	shopt -u dotglob
+	cd ${ZAMMAD_DIR}
+	bundle install --without test development
+	sed -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: postgresql\n#g' < config/database.yml.pkgr > config/database.yml
+	rake db:drop
+	rake db:create
+	rake db:migrate
+	rake db:seed
+	rake assets:precompile
+	rails r "Setting.set('es_url', 'http://elasticsearch:9200')"
+	rake searchindex:rebuild
+    fi
+
+    # delte logs & pids
+    rm ${ZAMMAD_DIR}/log/*
+    rm ${ZAMMAD_DIR}/tmp/pids/*
+
+    # run zammad
+    echo "starting zammad..."
+    script/websocket-server.rb -b 0.0.0.0 start &
+    script/scheduler.rb start &
+    rails s -p 3000 -b 0.0.0.0
+
+    if [ "${DEBUG}" == "yes" ]; then
+	# keepalive if error
+	while true; do
+    	    echo "debugging..."
+    	    sleep 600
+	done
+    fi
+
+fi
