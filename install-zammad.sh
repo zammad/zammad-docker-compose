2e0ed2265922646d2a946d13df0d4d0bf604ad19
Roy Kaldung <roy@kaldung.com>
2017-01-09 17:39:11 +0100

    fixed syntax, passed shellcheck

--- install-zammad.sh
+++ install-zammad.sh
@@ -2,15 +2,15 @@
 
 # install zammad
 echo "installing zammad..."
-cd /tmp
-git clone --depth 1 -b ${GIT_BRANCH} ${GIT_URL}
-cp -R /tmp/zammad/* ${ZAMMAD_DIR}
-cp -R /tmp/zammad/.[!.]* ${ZAMMAD_DIR}
-cd ${ZAMMAD_DIR}
+cd /tmp || exit 1
+git clone --depth 1 -b "${GIT_BRANCH}" "${GIT_URL}"
+cp -R /tmp/zammad/* "${ZAMMAD_DIR}"
+cp -R /tmp/zammad/.[!.]* "${ZAMMAD_DIR}"
+cd "${ZAMMAD_DIR}" || exit 1
 rm -rf /tmp/zammad
-bundle install --without test development mysql
+bundle install --without test development mysql \
 && contrib/packager.io/fetch_locales.rb
 sed -e 's#.*adapter: postgresql#  adapter: nulldb#g' -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: postgresql\n#g' < config/database.yml.pkgr > config/database.yml
 bundle exec rake assets:precompile
 sed -e 's#.*adapter: postgresql#  adapter: postgresql#g' -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: postgresql\n#g' < config/database.yml.pkgr > config/database.yml
-chown -R zammad:zammad ${ZAMMAD_DIR}
+chown -R zammad:zammad "${ZAMMAD_DIR}"
