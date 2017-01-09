e9e242ee645a89242eceaefa374c83231d04cf83
André Bauer <abauer@magix.net>
2017-01-09 18:51:09 +0100

    fixed db migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$1" = 'zammad' ]; then
 
     cd ${ZAMMAD_DIR}
-    bundle exec db:migrate &> /dev/null
+    bundle exec rake db:migrate &> /dev/null
 
     if [ $? != 0 ]; then
 	echo "creating db & searchindex..."
