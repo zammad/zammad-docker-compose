1403d48b8c30854a62aa567cc06d1ec203731d64
André Bauer <abauer@magix.net>
2017-01-10 11:46:26 +0100

    moved zammad user creation to install script, clone directly to /home without using /tmp

--- install-zammad.sh
+++ install-zammad.sh
@@ -2,18 +2,33 @@
 
 set -e
 
-# install zammad
 echo "installing zammad..."
-cd /tmp
+
+# crreate zammad user
+useradd -M -d /home/zammad -s /bin/bash zammad
+
+# # git clone zammad
+cd /home
 git clone --depth 1 -b "${GIT_BRANCH}" "${GIT_URL}"
-cp -R /tmp/zammad/* "${ZAMMAD_DIR}"
-cp -R /tmp/zammad/.[!.]* "${ZAMMAD_DIR}"
+
+# install zammad
 cd "${ZAMMAD_DIR}"
-rm -rf /tmp/zammad
 bundle install --without test development mysql
+
+# fetch locales
 contrib/packager.io/fetch_locales.rb
+
+# set nulldb database adapter for assets precompile
 sed -e 's#.*adapter: postgresql#  adapter: nulldb#g' -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: postgresql\n#g' < config/database.yml.pkgr > config/database.yml
+
+# assets precompile
 bundle exec rake assets:precompile
+
+# set postgresql database adapter
 sed -e 's#.*adapter: postgresql#  adapter: postgresql#g' -e 's#.*username:.*#  username: postgres#g' -e 's#.*password:.*#  password: \n  host: postgresql\n#g' < config/database.yml.pkgr > config/database.yml
+
+# delete assets precompile cache
 rm -r tmp/cache
+
+# set user & group to zammad
 chown -R zammad:zammad "${ZAMMAD_DIR}"
