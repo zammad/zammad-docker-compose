b6b0b058890b38afc21f1c597cc1154a8e2d7db0
Kersten Burkhardt <kersten@beyond-agentur.com>
2017-10-24 22:50:44 +0200

    Added backup once

--- containers/zammad-postgresql/backup.sh
+++ containers/zammad-postgresql/backup.sh
@@ -25,3 +25,24 @@ if [ "$1" = 'zammad-backup' ]; then
     sleep ${BACKUP_SLEEP}
   done
 fi
+
+if [ "$1" = 'zammad-backup-once' ]; then
+  # wait for zammad process coming up
+  until (echo > /dev/tcp/zammad-railsserver/3000) &> /dev/null; do
+    echo "backup waiting for zammads railsserver to be ready..."
+    sleep 2
+  done
+
+  TIMESTAMP="$(date +'%Y%m%d%H%M%S')"
+
+  echo "${TIMESTAMP} - backuping zammad..."
+
+  # delete old backups
+  test -d ${BACKUP_DIR} && find ${BACKUP_DIR}/*_zammad_*.gz -type f -mtime +${HOLD_DAYS} -exec rm {} \;
+
+  # tar files
+  tar -czf ${BACKUP_DIR}/${TIMESTAMP}_zammad_files.tar.gz ${ZAMMAD_DIR}
+
+  #db backup
+  pg_dump --dbname=postgresql://postgres@zammad-postgresql:5432/zammad_production | gzip > ${BACKUP_DIR}/${TIMESTAMP}_zammad_db.psql.gz
+fi
