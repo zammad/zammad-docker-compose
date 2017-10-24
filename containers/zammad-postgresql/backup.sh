69a3ed8a66e60e898fb6145f8e8b2149d7b7e687
Kersten Burkhardt <kersten@beyond-agentur.com>
2017-10-24 23:17:40 +0200

    removed double code

--- containers/zammad-postgresql/backup.sh
+++ containers/zammad-postgresql/backup.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-if [ "$1" = 'zammad-backup' ]; then
-  # wait for zammad process coming up
-  until (echo > /dev/tcp/zammad-railsserver/3000) &> /dev/null; do
-    echo "backup waiting for zammads railsserver to be ready..."
-    sleep 2
-  done
-
-  while true; do
-    TIMESTAMP="$(date +'%Y%m%d%H%M%S')"
+function backup {
+   TIMESTAMP="$(date +'%Y%m%d%H%M%S')"
 
     echo "${TIMESTAMP} - backuping zammad..."
 
@@ -20,6 +13,17 @@ if [ "$1" = 'zammad-backup' ]; then
 
     #db backup
     pg_dump --dbname=postgresql://postgres@zammad-postgresql:5432/zammad_production | gzip > ${BACKUP_DIR}/${TIMESTAMP}_zammad_db.psql.gz
+}
+
+if [ "$1" = 'zammad-backup' ]; then
+  # wait for zammad process coming up
+  until (echo > /dev/tcp/zammad-railsserver/3000) &> /dev/null; do
+    echo "backup waiting for zammads railsserver to be ready..."
+    sleep 2
+  done
+
+  while true; do
+    backup
 
     # wait until next backup
     sleep ${BACKUP_SLEEP}
@@ -33,16 +37,5 @@ if [ "$1" = 'zammad-backup-once' ]; then
     sleep 2
   done
 
-  TIMESTAMP="$(date +'%Y%m%d%H%M%S')"
-
-  echo "${TIMESTAMP} - backuping zammad..."
-
-  # delete old backups
-  test -d ${BACKUP_DIR} && find ${BACKUP_DIR}/*_zammad_*.gz -type f -mtime +${HOLD_DAYS} -exec rm {} \;
-
-  # tar files
-  tar -czf ${BACKUP_DIR}/${TIMESTAMP}_zammad_files.tar.gz ${ZAMMAD_DIR}
-
-  #db backup
-  pg_dump --dbname=postgresql://postgres@zammad-postgresql:5432/zammad_production | gzip > ${BACKUP_DIR}/${TIMESTAMP}_zammad_db.psql.gz
+  backup
 fi
