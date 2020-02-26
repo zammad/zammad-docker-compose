186bae4785ea043f90fbee4508881cbad761b942
André Bauer <monotek23@gmail.com>
2020-02-27 00:14:36 +0100

    test

--- .github/tests.sh
+++ .github/tests.sh
@@ -6,15 +6,16 @@
 set -o errexit
 set -o pipefail
 
-until (curl -I --silent --fail localhost | grep -iq zammad_session); do
+until (curl -I --silent --fail localhost | grep -iq "HTTP/1.1 200 OK"); do
     echo "wait for zammad to be ready..."
     sleep 15
 done
 
-docker exec -i zammad-docker-compose_zammad-railsserver_1 bash <<'EOF'
-set -o errexit
-bundle exec rails test test/unit/user_test.rb 
+echo "Zammad is up :)"
 
-EOF
+# docker exec -i zammad-docker-compose_zammad-railsserver_1 bash <<'EOF'
+# set -o errexit
+# bundle exec rails test test/unit/user_test.rb 
+# EOF
 
 docker-compose logs
