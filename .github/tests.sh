1f27af3273d221bdb248b67f70ee690463becad7
André Bauer <monotek23@gmail.com>
2020-02-27 00:28:45 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/tests.sh
+++ .github/tests.sh
@@ -6,28 +6,15 @@
 set -o errexit
 set -o pipefail
 
-#export TERM=dumb
-
 docker-compose logs --timestamps --follow &
 
 until (curl -I --silent --fail localhost | grep -iq "HTTP/1.1 200 OK"); do
     echo "wait for zammad to be ready..."
-    #docker-compose logs
     sleep 15
-    #clear
 done
 
+sleep 10
+
 echo
-echo "Zammad is up :)"
+echo "Success - Zammad is up :)"
 echo
-
-# echo
-# echo "Print logs"
-# echo
-
-# docker-compose logs
-
-# docker exec -i zammad-docker-compose_zammad-railsserver_1 bash <<'EOF'
-# set -o errexit
-# bundle exec rails test test/unit/user_test.rb 
-# EOF
