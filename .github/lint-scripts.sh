ca7a1fbb489fffcdcf14bff70d34d7daed07a4a4
André Bauer <monotek23@gmail.com>
2020-02-26 22:51:22 +0100

    added dockerlint

--- .github/lint-scripts.sh
+++ .github/lint-scripts.sh
@@ -5,12 +5,13 @@
 
 set -o errexit
 
-REPO_ROOT="$(git rev-parse --show-toplevel)"
 TMP_FILE="$(mktemp)"
 
-find "${REPO_ROOT}" -type f -name "*.sh" > "${TMP_FILE}"
+find . -type f -name "*.sh" > "${TMP_FILE}"
 
 while read -r FILE; do
   echo lint "${FILE}"
   shellcheck -x "${FILE}"
 done < "${TMP_FILE}"
+
+rm "${TMP_FILE}"
