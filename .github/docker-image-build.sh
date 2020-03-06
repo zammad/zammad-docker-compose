f93db7f2e8e7183266be95fdeb6ec6a2bee09d26
André Bauer <monotek@users.noreply.github.com>
2020-03-06 14:41:59 +0100

    added image build to ci & change db checks / migration (#142)
    * updated image to 3.3.0-11
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * add image build to ci / changed db cehcks
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added repo user var
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added needs
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * added executable bit
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * test
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * test
    
    Signed-off-by: André Bauer <monotek23@gmail.com>
    
    * test
    
    Signed-off-by: André Bauer <monotek23@gmail.com>

---
+++ .github/docker-image-build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# build zammads docker & docker-compose images
+
+set -o errexit
+set -o pipefail
+
+DOCKER_IMAGES="zammad zammad-elasticsearch zammad-postgresql"
+DOCKER_IMAGE_TAG="ci-snapshot"
+DOCKER_REGISTRY="index.docker.io"
+DOCKER_REPOSITORY="zammad-docker-compose"
+
+# dockerhub auth
+echo "${DOCKER_PASSWORD}" | docker login --username="${DOCKER_USERNAME}" --password-stdin
+
+# shellcheck disable=SC2153
+for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
+  echo "Build Zammad Docker image ${DOCKER_IMAGE} with version ${DOCKER_IMAGE_TAG} for DockerHubs ${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY} repo"
+ 
+  docker build --pull --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE}-${DOCKER_IMAGE_TAG}" -f "containers/${DOCKER_IMAGE}/Dockerfile" .
+  docker push "${DOCKER_REGISTRY}/${REPO_USER}/${DOCKER_REPOSITORY}:${DOCKER_IMAGE}-${DOCKER_IMAGE_TAG}"
+done
