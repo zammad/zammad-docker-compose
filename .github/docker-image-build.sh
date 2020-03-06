1e6006d72501ae09fb3466788843d085d62f311d
André Bauer <monotek23@gmail.com>
2020-03-06 15:19:15 +0100

    github action test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/docker-image-build.sh
+++ .github/docker-image-build.sh
@@ -13,7 +13,3 @@ for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
   echo "Build Zammad Docker image ${DOCKER_IMAGE} with version ${DOCKER_IMAGE_TAG} for local test"
   docker build --pull --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_IMAGE}-${DOCKER_IMAGE_TAG}" -f "containers/${DOCKER_IMAGE}/Dockerfile" .
 done
-
-# change images in compose file
-# shellcheck disable=SC2016
-sed -i -e 's#image: ${IMAGE_REPO}:##g' -e 's#${VERSION}#-ci-snapsoht#g'< docker-compose.yml
