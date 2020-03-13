7f96c8890b01d20dfb7ec2b23b140b95a2f64623
André Bauer <monotek23@gmail.com>
2020-03-13 02:15:44 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/tests.sh
+++ .github/tests.sh
@@ -32,20 +32,20 @@ echo
 echo "Fill db with some random data"
 docker exec zammad-docker-compose_zammad-railsserver_1 rails r "FillDB.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
 
-echo
-echo "create user via api"
-echo
-curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://localhost/api/v1/users'
+# echo
+# echo "create user via api"
+# echo
+# curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"firstname":"Bob","lastname":"Smith","email":"testuser@example.com","roles":["Customer"],"password":"some_password"}' 'http://localhost/api/v1/users'
 
-echo 
-echo "search user"
-echo
-curl --silent --fail --show-error -u info@zammad.org:Zammad 'http://localhost/api/v1/users/search?query=Smith&limit=10&expand=true'
+# echo 
+# echo "search user"
+# echo
+# curl --silent --fail --show-error -u info@zammad.org:Zammad 'http://localhost/api/v1/users/search?query=Smith&limit=10&expand=true'
 
-echo
-echo "create ticket"
-echo
-curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"title":"Help me!","group": "Users","article":{"subject":"some subject","body":"some message","type":"note","internal":false},"customer":"testuser@example.com","note": "some note"}' 'http://localhost/api/v1/tickets'
+# echo
+# echo "create ticket"
+# echo
+# curl --silent --fail --show-error -u info@zammad.org:Zammad -H "Content-Type: application/json" -X POST -d '{"title":"Help me!","group": "Users","article":{"subject":"some subject","body":"some message","type":"note","internal":false},"customer":"testuser@example.com","note": "some note"}' 'http://localhost/api/v1/tickets'
 
 
 
