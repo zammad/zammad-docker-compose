3e331c6957296d01c5927e6bd1a9aa6014027e1c
André Bauer <monotek23@gmail.com>
2020-03-13 01:51:36 +0100

    test
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- .github/tests.sh
+++ .github/tests.sh
@@ -29,7 +29,7 @@ echo
 
 sleep 10
 
-docker exec zammad-docker-compose_zammad-railsserver_1 rails 'r FillDB.load(agents: 50,customers: 1000,groups: 20,organizations: 40,overviews: 5,tickets: 100)'
+docker exec zammad-docker-compose_zammad-railsserver_1 rails 'r FillDB.load(agents: 10,customers: 10,groups: 10,organizations: 10,overviews: 10,tickets: 10)'
 
 echo
 echo "create user via api"
