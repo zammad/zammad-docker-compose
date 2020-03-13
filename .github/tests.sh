2f60d1fd14bd2cfb0b196f04a8b49d7376b4b5c1
André Bauer <monotek@users.noreply.github.com>
2020-03-13 14:01:02 +0100

    Update tests.sh

--- .github/tests.sh
+++ .github/tests.sh
@@ -18,21 +18,22 @@ sleep 30
 echo
 echo "Success - Zammad is up :)"
 echo
+
+echo 
 echo "Execute autowizard..."
 echo
 
-curl -I --silent --fail --show-error "http://localhost/#getting_started/auto_wizard/docker_compose_token" > /dev/null
+docker exec zammad-docker-compose_zammad-railsserver_1 rake zammad:setup:auto_wizard
 
 echo 
 echo "Autowizard executed successful :)"
 echo 
 
+
 echo 
-echo "Fill db with some random data..."
+echo "Fill DB with some random data"
+docker exec zammad-docker-compose_zammad-railsserver_1 rails r "FillDB.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
+
 echo
-
-docker exec zammad-docker-compose_zammad-railsserver_1 rails r "FillDB.load(agents: 10,customers: 10,groups: 10,organizations: 10,overviews: 10,tickets: 10)"
-
-echo 
-echo "Fill DB executed successful :)"
-echo 
+echo "DB fill successful :)"
+echo
