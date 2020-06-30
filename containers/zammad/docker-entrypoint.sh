f605d4519ea253cf39f71cfb7465dab8088ca0df
André Bauer <monotek23@gmail.com>
2020-06-30 14:58:40 +0200

    changed sed command

--- containers/zammad/docker-entrypoint.sh
+++ containers/zammad/docker-entrypoint.sh
@@ -117,7 +117,7 @@ if [ "$1" = 'zammad-nginx' ]; then
 
   # configure nginx
   # shellcheck disable=SC2154
-  sed -e "s#proxy_set_header X-Forwarded-Proto $scheme;#proxy_set_header X-Forwarded-Proto ${NGINX_SERVER_SCHEME};#g" \
+  sed -e "s#proxy_set_header X-Forwarded-Proto .*;#proxy_set_header X-Forwarded-Proto ${NGINX_SERVER_SCHEME};#g" \
       -e "s#server .*:3000#server ${ZAMMAD_RAILSSERVER_HOST}:${ZAMMAD_RAILSSERVER_PORT}#g" \
       -e "s#server .*:6042#server ${ZAMMAD_WEBSOCKET_HOST}:${ZAMMAD_WEBSOCKET_PORT}#g" \
       -e "s#server_name .*#server_name ${NGINX_SERVER_NAME};#g" \
