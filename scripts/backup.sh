5f0b9c75e10506a057c07ac55b3e71c0d0f6670c
Jensa <migasQ@users.noreply.github.com>
2023-05-12 17:01:28 +0200

    arithmetic linting fix

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -44,14 +44,14 @@ if [ "$1" = 'zammad-backup' ]; then
 
   while true; do
     NOW_TIMESTAMP=$(date +%s)
-    TOMORROW_DATE=$(date -d@"$(( $NOW_TIMESTAMP + 24*60*60 ))" +%Y-%m-%d)
+    TOMORROW_DATE=$(date -d@"$((NOW_TIMESTAMP + 24*60*60))" +%Y-%m-%d)
     
     zammad_backup
 
     NEXT_TIMESTAMP=$(date -d "$TOMORROW_DATE $BACKUP_TIME" +%s)
     NOW_TIMESTAMP=$(date +%s)
 
-    sleep $(( $NEXT_TIMESTAMP - $NOW_TIMESTAMP ))
+    sleep $((NEXT_TIMESTAMP - NOW_TIMESTAMP))
   done
 fi
 
