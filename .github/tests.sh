944e05a9208dee0de54d35cd1607f653ed226bba
Martin Gruner <mg@zammad.com>
2024-04-29 10:55:13 +0200

    Maintenance: Improve CI test script to use 'bundle exec' and wait less.

--- .github/tests.sh
+++ .github/tests.sh
@@ -10,10 +10,8 @@ docker-compose logs --timestamps --follow &
 
 until (curl -I --silent --fail localhost:8080 | grep -iq "HTTP/1.1 200 OK"); do
     echo "wait for zammad to be ready..."
-    sleep 15
+    sleep 5
 done
-
-sleep 30
 
 echo
 echo "Success - Zammad is up :)"
@@ -23,7 +21,7 @@ echo
 echo "Execute autowizard..."
 echo
 
-docker exec --env=AUTOWIZARD_RELATIVE_PATH=tmp/auto_wizard.json --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 rake zammad:setup:auto_wizard
+docker exec --env=AUTOWIZARD_RELATIVE_PATH=tmp/auto_wizard.json --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 bundle exec rake zammad:setup:auto_wizard
 
 echo
 echo "Autowizard executed successful :)"
@@ -34,7 +32,7 @@ echo
 echo "Check DB for AutoWizard user"
 echo
 
-docker exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 rails r "p User.find_by(email: 'info@zammad.org')" | grep 'info@zammad.org'
+docker exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 bundle exec rails r "p User.find_by(email: 'info@zammad.org')" | grep 'info@zammad.org'
 
 echo
 echo "Check DB for AutoWizard user successfull :)"
@@ -44,7 +42,7 @@ echo
 echo "Fill DB with some random data"
 echo
 
-docker exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 rails r "FillDb.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
+docker exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-docker-compose_zammad-railsserver_1 bundle exec rails r "FillDb.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
 
 echo
 echo "DB fill successful :)"
