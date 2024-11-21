bedc5a2d48dbb87e543f3d56e71e0c4c0ca166b7
Martin Gruner <mg@zammad.com>
2024-11-21 10:27:06 +0100

    Fixes #440 - zammad-backup not working with postgres 17

--- .github/tests.sh
+++ .github/tests.sh
@@ -6,55 +6,38 @@
 set -o errexit
 set -o pipefail
 
+# Send the logs to STDOUT for debugging.
 docker compose logs --timestamps --follow &
 
-echo
-echo "wait for zammad to be ready..."
-echo
+# Print empty lines before and after the heading to find it between the logs.
+print_heading() {
+  echo
+  echo "$1"
+  echo
+}
 
+print_heading "wait for zammad to be ready..."
 docker compose wait zammad-init
 curl --retry 30 --retry-delay 1 --retry-connrefused http://localhost:8080 | grep "Zammad"
+print_heading "Success - Zammad is up :)"
 
-echo
-echo "Success - Zammad is up :)"
-echo
-
-echo
-echo "Execute autowizard..."
-echo
-
+print_heading "Execute autowizard..."
 docker compose exec --env=AUTOWIZARD_RELATIVE_PATH=tmp/auto_wizard.json --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-railsserver bundle exec rake zammad:setup:auto_wizard
+print_heading "Autowizard executed successful :)"
 
-echo
-echo "Autowizard executed successful :)"
-echo
-
-echo
-echo "Check DB for AutoWizard user"
-echo
-
+print_heading "Check DB for AutoWizard user"
 docker compose exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-railsserver bundle exec rails r "p User.find_by(email: 'info@zammad.org')" | grep 'info@zammad.org'
+print_heading "Check DB for AutoWizard user successfull :)"
 
-echo
-echo "Check DB for AutoWizard user successfull :)"
-echo
-
-echo
-echo "Fill DB with some random data"
-echo
-
+print_heading "Fill DB with some random data"
 docker compose exec --env=DATABASE_URL=postgres://zammad:zammad@zammad-postgresql:5432/zammad_production zammad-railsserver bundle exec rails r "FillDb.load(agents: 1,customers: 1,groups: 1,organizations: 1,overviews: 1,tickets: 1)"
+print_heading "DB fill successful :)"
 
-echo
-echo "DB fill successful :)"
-echo
-
-echo
-echo "Check if the Zammad user can write to FS storage"
-echo
-
+print_heading "Check if the Zammad user can write to FS storage"
 docker compose exec zammad-railsserver touch storage/test.txt
+print_heading "Storage write successful :)"
 
-echo
-echo "Storage write successful :)"
-echo
+print_heading "Check if zammad-backup created a backup"
+docker compose exec zammad-backup sh -c "ls /var/tmp/zammad/* | grep zammad_db.psql.gz"
+docker compose exec zammad-backup sh -c "ls /var/tmp/zammad/* | grep zammad_files.tar.gz"
+print_heading "Zammad backup was created"
