091e84843c0b38ce8974d8cf2915a2be84d7de45
Martin Gruner <mg@zammad.com>
2025-03-05 14:10:07 +0100

    Make exposed ES HTTP port configurable

--- .github/tests/add-hostport-to-elasticsearch.sh
+++ .github/tests/add-hostport-to-elasticsearch.sh
@@ -6,5 +6,5 @@
 check_stack_start
 
 print_heading "check for hostport"
-docker inspect zammad-docker-compose-zammad-elasticsearch-1 | grep HostPort
+docker inspect zammad-docker-compose-zammad-elasticsearch-1 | grep HostPort | grep 9201
 print_heading "Success - hostport is present"
--- .github/tests/setup/add-hostport-to-elasticsearch.sh
+++ .github/tests/setup/add-hostport-to-elasticsearch.sh
@@ -2,4 +2,7 @@
 
 set -o errexit
 
+# Use a custom port to verify it is configurable.
+echo "ELASTICSEARCH_EXPOSE_HTTP_PORT=9201" > .env
+
 docker compose -f docker-compose.yml -f scenarios/add-hostport-to-elasticsearch.yml up --quiet-pull --detach
