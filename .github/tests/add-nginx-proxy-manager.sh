aa31af5f9813f618868aaeb9d7666cc7b36a5fcc
Martin Gruner <mg@zammad.com>
2026-02-16 10:35:46 +0100

    Improve matching

--- .github/tests/add-nginx-proxy-manager.sh
+++ .github/tests/add-nginx-proxy-manager.sh
@@ -12,5 +12,6 @@ print_heading "Success - nginx-proxy-manager container is present"
 
 print_heading "check trusted proxy configuration for nginx-proxy-manager"
 railsserver_run_command bundle exec rails r "pp Zammad::TrustedProxies.fetch"
-railsserver_run_command bundle exec rails r "Zammad::TrustedProxies.fetch.count.one?"
+railsserver_run_command bundle exec rails r "Zammad::TrustedProxies.fetch == [Resolv.getaddress('nginx-proxy-manager').to_s] || abort('nginx-proxy-manager is not in trusted proxies')"
+railsserver_run_command bundle exec rails r "Zammad::TrustedProxies.fetch == [Resolv.getaddress('nginx-proxy-manager2').to_s] || abort('debug')"
 print_heading "Success- check trusted proxy configuration for nginx-proxy-manager"
