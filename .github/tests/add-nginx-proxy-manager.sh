49be1a797c3a0729a79daf27ea8508a4b5cb4edc
Martin Gruner <mg@zammad.com>
2026-02-16 10:22:47 +0100

    Automatically configure trusted RAILS_TRUSTED_PROXIES for proxy scenarios

--- .github/tests/add-nginx-proxy-manager.sh
+++ .github/tests/add-nginx-proxy-manager.sh
@@ -8,3 +8,9 @@ check_stack_start
 print_heading "check for presence nginx-proxy-manager container"
 docker compose ps | grep nginx-proxy-manager
 print_heading "Success - nginx-proxy-manager container is present"
+
+
+print_heading "check trusted proxy configuration for nginx-proxy-manager"
+railsserver_run_command bundle exec rails r "pp Zammad::TrustedProxies.fetch"
+railsserver_run_command bundle exec rails r "Zammad::TrustedProxies.fetch.count.one?"
+print_heading "Success- check trusted proxy configuration for nginx-proxy-manager"
