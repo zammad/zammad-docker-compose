850c1740746f6e17a400c395997cfc1f9055cc79
Moltivie <major2015usa@gmail.com>
2026-03-11 07:29:09 +0100

    Fixes #534 - Reset exposed ports for nginx-proxy-manager and cloudflare-tunnel scenarios (#545)

--- .github/tests/add-cloudflare-tunnel.sh
+++ .github/tests/add-cloudflare-tunnel.sh
@@ -8,3 +8,7 @@ check_stack_start
 print_heading "check for presence cloudflare tunnel container"
 docker compose ps | grep cloudflare-tunnel
 print_heading "Success - cloudflare container is present"
+
+print_heading "check that nginx is not exposed on the Host"
+docker inspect zammad-docker-compose-zammad-nginx-1 | grep HostPort && exit 1
+print_heading "Success - nginx is not exposed on the host"
--- .github/tests/add-nginx-proxy-manager.sh
+++ .github/tests/add-nginx-proxy-manager.sh
@@ -10,6 +10,10 @@ docker compose ps | grep nginx-proxy-manager
 print_heading "Success - nginx-proxy-manager container is present"
 
 
+print_heading "check that nginx is not exposed on the Host"
+docker inspect zammad-docker-compose-zammad-nginx-1 | grep HostPort && exit 1
+print_heading "Success - nginx is not exposed on the host"
+
 print_heading "check trusted proxy configuration for nginx-proxy-manager"
 railsserver_run_command bundle exec rails r 'puts "Trusted Proxies configured as: #{Zammad::TrustedProxies.fetch.inspect}"'
 railsserver_run_command bundle exec rails r "Zammad::TrustedProxies.fetch == [Resolv.getaddress('nginx-proxy-manager').to_s] || abort('nginx-proxy-manager is not in trusted proxies')"
