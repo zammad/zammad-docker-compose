18437afe1b0621eba62a76608ab35b3342334e79
Dusan Vuckovic <dv@zammad.com>
2026-09-15 18:27:12 +0100

    chore: Reorder stack start.

--- .github/tests/setup/add-libretranslate.sh
+++ .github/tests/setup/add-libretranslate.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
+docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
+
 # Wait for libretranslate to be ready (downloading the models may take several minutes).
 docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
-
-docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
