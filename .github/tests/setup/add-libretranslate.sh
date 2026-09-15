d46d25b2a53fb2856c7f6499dfb8a6ebba50a9fd
Dusan Vuckovic <dv@zammad.com>
2026-09-15 17:58:50 +0100

    yolo:

--- .github/tests/setup/add-libretranslate.sh
+++ .github/tests/setup/add-libretranslate.sh
@@ -4,3 +4,5 @@ set -o errexit
 
 # Wait for libretranslate to be ready (downloading the models may take several minutes).
 docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
+
+docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
