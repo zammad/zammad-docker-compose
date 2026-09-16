1a7fa663a1c034e06630c7cb8cbeab065cab5e89
Dusan Vuckovic <dv@zammad.com>
2026-09-16 08:56:57 +0100

    test: Try to speed up stack startup.

--- .github/tests/setup/add-libretranslate.sh
+++ .github/tests/setup/add-libretranslate.sh
@@ -2,7 +2,5 @@
 
 set -o errexit
 
-# Wait for libretranslate to be ready (downloading the models may take several minutes).
-docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
-
-docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
+# Load only English (en) language in order to speed up the stack startup.
+LT_LOAD_ONLY=en docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
