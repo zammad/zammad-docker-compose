14d4544befd7366865c91405bd939163fa053e30
Dusan Vuckovic <dv@zammad.com>
2026-09-16 09:36:47 +0100

    test: Another try.

--- .github/tests/setup/add-libretranslate.sh
+++ .github/tests/setup/add-libretranslate.sh
@@ -2,8 +2,10 @@
 
 set -o errexit
 
-# Wait for libretranslate to be ready, as downloading the initialization may take some time.
-#   Load only English (en) language in order to speed up the stack startup.
-LT_LOAD_ONLY=en docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
+# Load only English and German language models in order to speed up the stack startup.
+export LT_LOAD_ONLY="en,de"
 
-LT_LOAD_ONLY=en docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
+# Wait for libretranslate to be ready, as downloading the initialization may take some time.
+docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
+
+docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
