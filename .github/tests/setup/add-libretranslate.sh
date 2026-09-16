c0ac60b996a3449f45e9b0d13ebe8f2dc2420df8
Dusan Vuckovic <dv@zammad.com>
2026-09-16 11:16:48 +0100

    fix: Address review findings.

--- .github/tests/setup/add-libretranslate.sh
+++ .github/tests/setup/add-libretranslate.sh
@@ -6,6 +6,6 @@ set -o errexit
 export LT_LOAD_ONLY="en,de"
 
 # Wait for libretranslate to be ready, as downloading the initialization may take some time.
-docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 900 libretranslate
+docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach --wait --wait-timeout 300 libretranslate
 
 docker compose -f docker-compose.yml -f scenarios/add-libretranslate.yml up --detach
